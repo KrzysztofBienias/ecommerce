a1d8f23000cc76f167a9721e93b2ca5ce3826d2c
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2022-11-02 17:23:23 +0100

    Added hover effect on footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,9 +4,9 @@ import { ImArrowUpRight2 } from 'react-icons/im';
 const Footer = () => {
     return (
         <footer className="mt-20 md:mt-32">
-            <p className="2xl: pr-2 text-right md:pr-2 2xl:pr-0">Created only for educational purpose</p>
+            <p className="text-right md:pr-2 2xl:pr-0">Created only for educational purpose</p>
             <h3 className="border-y border-gray-300 text-[10.5vw] font-bold 2xl:text-[161px]">
-                <a className="flex items-center" href="https://www.behance.net/nelsondearaujodesign">
+                <a className="flex items-center hover:italic" href="https://www.behance.net/nelsondearaujodesign">
                     Nelson de Araújo
                     <ImArrowUpRight2 />
                 </a>
