febe0b7cd5b0a545043b8297b1db2932edf43273
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2022-12-20 23:43:04 +0100

    :sparkles: Animated header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,6 +3,27 @@ import { useSession } from 'next-auth/react';
 import { useSelector } from 'react-redux';
 import { selectItems } from '../redux/slices/basksetSlice';
 import Link from './link';
+import { motion } from 'framer-motion';
+
+const navVariant = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { staggerChildren: 0.1 } },
+};
+
+const logoVariant = {
+    hidden: { y: 20, opacity: 0 },
+    show: { y: 0, opacity: 1, transition: { staggerChildren: 0.1, delay: 0.4 } },
+};
+
+const subLogoWrapperVariant = {
+    hidden: { y: 20, opacity: 0 },
+    show: { y: 0, opacity: 1, transition: { staggerChildren: 0.1, delay: 0.6 } },
+};
+
+const subLogoVariant = {
+    hidden: { y: 20, opacity: 0 },
+    show: { y: 0, opacity: 1 },
+};
 
 const Header = () => {
     const { data: session } = useSession();
@@ -10,26 +31,40 @@ const Header = () => {
 
     return (
         <header>
-            <nav className="flex justify-between px-5 py-6 text-sm min-[440px]:text-base sm:px-14 2xl:px-0">
+            <motion.nav
+                variants={navVariant}
+                initial="hidden"
+                animate="show"
+                className="flex justify-between px-5 py-6 text-sm min-[440px]:text-base sm:px-14 2xl:px-0"
+            >
                 <Link route="/">Home</Link>
-                <div className="text-center min-[360px]:flex ">
+                <motion.div variants={navVariant} className="text-center min-[360px]:flex">
                     <Link isSignIn={session ? true : false}>{session ? 'Sign out' : 'Sign in'}</Link>
                     <Link route="/profile">Profile</Link>
                     <Link route="/products">Products</Link>
-                </div>
-                <Link route="/cart">{`Cart(${items.length})`}</Link>
-            </nav>
+                </motion.div>
+                <Link route="/cart" delay={0.3}>{`Cart(${items.length})`}</Link>
+            </motion.nav>
 
             <div className="mx-auto my-5 max-w-fit">
-                <div className="border-y border-gray-300">
+                <motion.div variants={logoVariant} initial="hidden" animate="show" className="border-y border-gray-300">
                     <h2 className="py-4 text-center text-5xl font-bold min-[440px]:text-7xl min-[600px]:text-8xl md:text-9xl xl:text-[200px]">
                         THE ABCDE
                     </h2>
-                </div>
-                <div className="mb-8 flex justify-between border-b-2 border-gray-700 px-6 py-2">
-                    <p className="text-xs">High-End Furniture</p>
-                    <p className="text-xs">SINCE 1978</p>
-                </div>
+                </motion.div>
+                <motion.div
+                    variants={subLogoWrapperVariant}
+                    initial="hidden"
+                    animate="show"
+                    className="mb-8 flex justify-between border-b-2 border-gray-700 px-6 py-2"
+                >
+                    <motion.p variants={subLogoVariant} className="text-xs">
+                        High-End Furniture
+                    </motion.p>
+                    <motion.p variants={subLogoVariant} className="text-xs">
+                        SINCE 1978
+                    </motion.p>
+                </motion.div>
             </div>
         </header>
     );
--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,13 +1,15 @@
 import { useRouter } from 'next/router';
 import { signIn, signOut } from 'next-auth/react';
+import { motion } from 'framer-motion';
 
 interface LinkProps {
     children: React.ReactNode;
     route?: string;
     isSignIn?: boolean;
+    delay?: number;
 }
 
-const Link = ({ children, route, isSignIn }: LinkProps) => {
+const Link = ({ children, route, isSignIn, delay }: LinkProps) => {
     const router = useRouter();
     const onClickHandler = () => {
         if (route) return router.push(route);
@@ -15,12 +17,23 @@ const Link = ({ children, route, isSignIn }: LinkProps) => {
         else if (!isSignIn) return signIn();
     };
 
+    let transition = {
+        hidden: { y: 20, opacity: 0 },
+        show: { y: 0, opacity: 1 },
+    };
+
+    if (delay) Object.assign(transition.show, { transition: { delay } });
+
     return (
-        <a className="group cursor-pointer p-2 hover:font-bold sm:p-4 lg:p-6" onClick={onClickHandler}>
+        <motion.a
+            className="group cursor-pointer p-2 hover:font-bold sm:p-4 lg:p-6"
+            onClick={onClickHandler}
+            variants={transition}
+        >
             <span className="relative inline-block overflow-hidden before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-full before:origin-[100%_50%] before:scale-x-0 before:bg-gray-700 before:transition-transform before:duration-300 before:ease-[cubic-bezier(.76,0,.24,1)] focus:before:origin-[0%_50%] focus:before:scale-x-100  group-hover:before:origin-[0%_50%] group-hover:before:scale-x-100">
                 {children}
             </span>
-        </a>
+        </motion.a>
     );
 };
 
