3c8b7caa90cc65c9f6fb1b164c074529f17b285a
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2022-12-23 14:59:16 +0100

    :sparkles: Created link component and animted it

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -17,18 +17,18 @@ const Link = ({ children, route, isSignIn, delay }: LinkProps) => {
         else if (!isSignIn) return signIn();
     };
 
-    let transition = {
+    let linkVariant = {
         hidden: { y: 20, opacity: 0 },
         show: { y: 0, opacity: 1 },
     };
 
-    if (delay) Object.assign(transition.show, { transition: { delay } });
+    if (delay) Object.assign(linkVariant.show, { transition: { delay } });
 
     return (
         <motion.a
             className="group cursor-pointer p-2 hover:font-bold sm:p-4 lg:p-6"
             onClick={onClickHandler}
-            variants={transition}
+            variants={linkVariant}
         >
             <span className="relative inline-block overflow-hidden before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-full before:origin-[100%_50%] before:scale-x-0 before:bg-gray-700 before:transition-transform before:duration-300 before:ease-[cubic-bezier(.76,0,.24,1)] focus:before:origin-[0%_50%] focus:before:scale-x-100  group-hover:before:origin-[0%_50%] group-hover:before:scale-x-100">
                 {children}
