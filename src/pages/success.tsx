6f044c4bdc07c0ca4e11e95afaf2e7eeb2e70226
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2022-12-25 12:58:29 +0100

    :sparkles: Animated success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,17 +1,25 @@
 import type { NextPage } from 'next';
-import { useRouter } from 'next/router';
 import Footer from '../components/footer';
 import Header from '../components/header';
 import Link from './../components/link';
+import { motion } from 'framer-motion';
+
+const wrapperVariant = {
+    hidden: { opacity: 0, y: 100 },
+    show: { opacity: 1, y: 0, transition: { delay: 0.8 } },
+};
 
 const Success: NextPage = () => {
-    const router = useRouter();
-
     return (
         <div className="mx-auto flex min-h-screen max-w-screen-2xl flex-col font-montserrat">
             <Header />
 
-            <div className="align-center flex flex-1 flex-col items-center justify-center space-y-5 p-6 text-center">
+            <motion.div
+                variants={wrapperVariant}
+                initial="hidden"
+                animate="show"
+                className="align-center flex flex-1 flex-col items-center justify-center space-y-5 p-6 text-center"
+            >
                 <h1 className="text-xl font-bold sm:text-3xl md:text-4xl xl:text-5xl 2xl:pb-4 2xl:text-6xl">
                     Thank you, your order has been confirmed!
                 </h1>
@@ -20,7 +28,7 @@ const Success: NextPage = () => {
                     to check the status of your order(s) please press the link below.
                 </p>
                 <Link route="/profile">Profile</Link>
-            </div>
+            </motion.div>
 
             <Footer />
         </div>
