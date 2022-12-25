559de49ac0f9260d9af6a43a19b8b82196322d25
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2022-12-25 12:57:41 +0100

    :sparkles: Animated cart page

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -6,6 +6,7 @@ import { selectItems, selectTotal } from '../redux/slices/basksetSlice';
 import { useSession } from 'next-auth/react';
 import { loadStripe } from '@stripe/stripe-js';
 import axios from 'axios';
+import { motion } from 'framer-motion';
 
 const stripePromise = loadStripe(process.env.stripe_public_key as string);
 
@@ -25,23 +26,52 @@ const Cart = () => {
         if (result?.error) alert(result.error.message);
     };
 
+    const checkoutProductVariant = {
+        hidden: { opacity: 0 },
+        show: { opacity: 1, transition: { when: 'beforeChildren', staggerChildren: 0.2, duration: 0.5, delay: 0.2 } },
+    };
+
+    const infoVariant = {
+        hidden: { y: 50, opacity: 0 },
+        show: { y: 0, opacity: 1, transition: { duration: 0.4, delay: 1 } },
+    };
+
     return (
         <div className="mx-auto flex min-h-screen max-w-screen-2xl flex-col font-montserrat">
             <Header />
 
             <div className="flex w-full max-w-6xl flex-1 flex-col justify-between sm:px-10 md:mx-auto md:mt-14 md:flex-row xl:mt-24 xl:px-0">
-                <div className="order-2 pl-2 md:order-1 md:w-2/3 md:max-w-3xl md:pl-0">
+                <motion.div
+                    variants={checkoutProductVariant}
+                    initial="hidden"
+                    animate="show"
+                    className="order-2 pl-2 md:order-1 md:w-2/3 md:max-w-3xl md:pl-0"
+                >
                     {items.map((item, index) => (
                         <CheckoutProduct item={item} key={index} />
                     ))}
-                </div>
+                </motion.div>
 
                 <div className="order-1 md:order-2">
-                    <div className="p-4 text-right sm:p-0 sm:py-4">
-                        <p className="text-xl font-bold lg:text-2xl xl:text-4xl">Total price: ${total}</p>
-                        <p className="lg:text-lg xl:text-2xl">items: {items.length}</p>
-                        <p>(max 5 items)</p>
-                        <button
+                    <motion.div transition={{ staggerChildren: 0.1 }} className="p-4 text-right sm:p-0 sm:py-4">
+                        <motion.p
+                            variants={infoVariant}
+                            initial="hidden"
+                            animate="show"
+                            className="text-xl font-bold lg:text-2xl xl:text-4xl"
+                        >
+                            Total price: ${total}
+                        </motion.p>
+                        <motion.p variants={infoVariant} initial="hidden" animate="show" className="lg:text-lg xl:text-2xl">
+                            items: {items.length}
+                        </motion.p>
+                        <motion.p variants={infoVariant} initial="hidden" animate="show">
+                            (max 5 items)
+                        </motion.p>
+                        <motion.button
+                            variants={infoVariant}
+                            initial="hidden"
+                            animate="show"
                             role="link"
                             disabled={!session || items.length === 0}
                             onClick={createCheckoutSession}
@@ -49,11 +79,16 @@ const Cart = () => {
                                 !session || items.length === 0 ? 'cursor-not-allowed' : null
                             }`}
                         >
-                            <span className="relative inline-block overflow-hidden before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-full before:origin-[100%_50%] before:scale-x-0 before:bg-gray-700 before:transition-transform before:duration-300 before:ease-[cubic-bezier(.76,0,.24,1)] focus:before:origin-[0%_50%] focus:before:scale-x-100  group-hover:before:origin-[0%_50%] group-hover:before:scale-x-100">
+                            <motion.span
+                                variants={infoVariant}
+                                initial="hidden"
+                                animate="show"
+                                className="relative inline-block overflow-hidden before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-full before:origin-[100%_50%] before:scale-x-0 before:bg-gray-700 before:transition-transform before:duration-300 before:ease-[cubic-bezier(.76,0,.24,1)] focus:before:origin-[0%_50%] focus:before:scale-x-100  group-hover:before:origin-[0%_50%] group-hover:before:scale-x-100"
+                            >
                                 {!session ? 'Sign in to checkout' : 'Proceed to checkout'}
-                            </span>
-                        </button>
-                    </div>
+                            </motion.span>
+                        </motion.button>
+                    </motion.div>
                 </div>
             </div>
 
