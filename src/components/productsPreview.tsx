efbb054f9c368d14b2caa0ba2405cb2e869c450c
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2023-05-28 17:35:53 +0200

    ⚡ Fetched data inside component instead of passing props

--- src/components/productsPreview.tsx
+++ src/components/productsPreview.tsx
@@ -1,17 +1,17 @@
-import Product from './product';
-import type { ProductT } from '../types';
-import { motion } from 'framer-motion';
+'use client';
 
-interface ProductsPreviewI {
-    products: ProductT[];
-}
+import Product from './product';
+import { motion } from 'framer-motion';
+import { useGetAllProductsQuery } from '../store/api/productsApi';
 
 const productsPreviewVariant = {
     hidden: { opacity: 0 },
     show: { opacity: 1, transition: { staggerChildren: 0.08, delay: 0.4, duration: 0.4 } },
 };
 
-const ProductsPreview: React.FC<ProductsPreviewI> = ({ products }) => {
+const ProductsPreview = () => {
+    const { isLoading, isFetching, data, error } = useGetAllProductsQuery(null);
+
     return (
         <motion.div
             variants={productsPreviewVariant}
@@ -20,9 +20,7 @@ const ProductsPreview: React.FC<ProductsPreviewI> = ({ products }) => {
             viewport={{ once: true }}
             className="grid grid-cols-1 xs:grid-cols-2 xs:grid-rows-2 xs:gap-2 md:grid-cols-4 md:grid-rows-none"
         >
-            {products.slice(0, 4).map((product) => (
-                <Product product={product} key={product.id} />
-            ))}
+            {data && data.slice(0, 4).map((product) => <Product product={product} key={product.id} />)}
         </motion.div>
     );
 };
